04e6de24779c1af3ce7bdb2d241a30ed2794f659
LucianoFromTrelew <lucianoserruya@hotmail.com>
2016-09-06 18:12:45 -0300

    pilas del ej3 funcionando

--- practico2/ej3.c
+++ practico2/ej3.c
@@ -3,8 +3,9 @@
 #include<unistd.h>
 #include<stdlib.h>
 
-#define MAX_PILA 1000
+#define MAX_PILA  1000
 #define MAX_HILOS 10
+#define ESPERA    50000
 
 pthread_t hilosPush[MAX_HILOS];
 pthread_t hilosPop[MAX_HILOS];
@@ -16,7 +17,7 @@ typedef struct {
     int contenido[MAX_PILA];
 } pila_t;
 
-pila_t pila  = {0, PTHREAD_COND_INITIALIZER, PTHREAD_COND_INITIALIZER, PTHREAD_MUTEX_INITIALIZER};
+pila_t pila  = {-1, PTHREAD_COND_INITIALIZER, PTHREAD_COND_INITIALIZER, PTHREAD_MUTEX_INITIALIZER};
     
 int numeroAleatorio(){
     return rand() % 10;
@@ -24,11 +25,13 @@ int numeroAleatorio(){
 void *push (void *arg){
     while(1){
         pthread_mutex_lock(&pila.mutex);
-        if(pila.tope == MAX_PILA-1){
+        if(pila.tope == (MAX_PILA-1)){
             pthread_cond_wait(&pila.llena,&pila.mutex);
+            printf("PILA_LLENA: Espero condicion\n");
         }
         //el tope apunta al elemento visible de la pila
         pila.contenido[++pila.tope] = numeroAleatorio();
+        
         pthread_cond_broadcast(&pila.vacia);
         
         pthread_mutex_unlock(&pila.mutex);
@@ -42,11 +45,13 @@ void *pop (void *arg){
         pthread_mutex_lock(&pila.mutex);
         if(pila.tope == -1){
             pthread_cond_wait(&pila.vacia,&pila.mutex);
+            printf("PILA_VACIA: Espero condicion\n");
         }
         //el tope apunta al elemento visible de la pila
         printf("%d\n", pila.contenido[pila.tope]);
-        usleep(50000);
+        usleep(ESPERA);
         pila.tope--;
+        
         pthread_cond_broadcast(&pila.llena);
         
         pthread_mutex_unlock(&pila.mutex);
@@ -71,12 +76,16 @@ int main(void){
             exit(1);
     }
     
+    sleep(1);
+    
     //hilos popeadores
     for(i = 0; i < MAX_HILOS ; i++){
         retval = pthread_create(&hilosPop[i],NULL,&pop, NULL);
         if(retval != 0)
             exit(1);
     }
+    
+    
     //esperamos al primer hilo creado
     pthread_join(hilosPush[0], NULL);
     return 0;
